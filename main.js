537a995d3205f9bf03731f195ccbb7f2c9df5964
Vitalii Bukatin <bukatin93@gmail.com>
2023-09-06 14:33:30 +0300

    completed hw

---
+++ main.js
@@ -0,0 +1,6 @@
+let first = prompt('Введите первое число')
+let second = prompt('Введите второе число')
+
+alert(
+	`${first} + ${second} = ${Number(first) + Number(second)}\n${first} - ${second} = ${Number(first) - Number(second)}\n${first} * ${second} = ${Number(first) * Number(second)}\n${first} / ${second} = ${Number(first) / Number(second)}`
+)
